c47125277bad311ebc650215dc04e20e44e727ea
Kevin <163057317+Katania91@users.noreply.github.com>
2025-07-25 15:36:02 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Katania91'
+description 'A standalone script that allows players to toggle their hair on and off.'
+version '1.0'
+
+client_script 'client.lua'
